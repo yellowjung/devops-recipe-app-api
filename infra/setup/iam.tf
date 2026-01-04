b8f0eb91fd33c5904b8cd6370547a3869e7d9bea
youngdon <yeavov@gmail.com>
2026-01-04 15:43:43 +0900

    added action for create lb

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -135,6 +135,7 @@ data "aws_iam_policy_document" "ec2" {
       "ec2:AttachInternetGateway",
       "ec2:ModifyVpcAttribute",
       "ec2:RevokeSecurityGroupIngress",
+      "ec2:DescribeAccountAttributes"
     ]
     resources = ["*"]
   }
