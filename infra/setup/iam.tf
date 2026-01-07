5d05e7239cc7dc629521e053a42eda09ef4a8903
youngdon <yeavov@gmail.com>
2026-01-07 15:30:37 +0900

    added route 53 delete action

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -394,6 +394,7 @@ data "aws_iam_policy_document" "route53" {
       "route53:GetChange",
       "route53:ListResourceRecordSets",
       "route53:CreateHostedZone",
+      "route53:DeleteHostedZone",
       "acm:RequestCertificate",
       "acm:AddTagsToCertificate",
       "acm:DescribeCertificate",
